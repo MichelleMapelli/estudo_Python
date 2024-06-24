f2a359b1c8bbf9041be7436caf0fc53706d08099
Michelle Trigueiro <mi.mapelli@gmail.com>
2024-06-24 14:01:03 -0300

    Concluindo game

--- projeto_jogo_da_forca.py
+++ projeto_jogo_da_forca.py
@@ -41,4 +41,40 @@ def game():
    # Lista para as letras erradas
    letras_erradas = []
 
+   # Loop enquanto o número de cances for maior do que zero
+   while chances > 0:
+      
+      # Print
+      print(" ".join(letras_descobertas))
+      print("\nChances restantes:", chances)
+      print("Letras erradas:", " ".join(letras_erradas))
+
+      # Tentativa
+      tentativa = input("\nDigite uma letra: ").lower()
+
+      # Condicional
+      if tentativa in palavra:
+         index = 0
+        
+         for letra in palavra:
+            if tentativa == letra:
+               letras_descobertas[index] = letra
+            index += 1
+      else:
+         chances -= 1
+         letras_erradas.append(tentativa)
+
+      # Condicional
+      if "_" not in letras_descobertas:
+         print("\nVocê venceu, a palavra era:", palavra)
+         break
+      
+   # Condicional
+   if "_" in letras_descobertas:
+      print("\nVocê perdeu, a palavra era", palavra)
+
+# Bloco main
+if __name__ == "__main__":
+   game()
+   print("\nParabéns, Você está aprendendo programação em Python com a DSA. :)\n")
 
